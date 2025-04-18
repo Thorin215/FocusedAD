bdb9c7f3376beaaa1c82b8d17fe2a2e402a8422d
Daria8976 <105217681+Daria8976@users.noreply.github.com>
2025-04-18 10:54:57 +0800

    Add files via upload

---
+++ environment.sh
@@ -0,0 +1,7 @@
+pip install -r requirements.txt
+pip install flash-attn==2.5.8 --no-build-isolation
+pip install facenet-pytorch scikit-learn pandas numpy matplotlib
+
+pip install -U huggingface_hub
+export HF_ENDPOINT=https://hf-mirror.com
+huggingface-cli download --resume-download DAMO-NLP-SG/VideoRefer-7B --local-dir ./checkpoints
